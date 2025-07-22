cf3ecc2abd5d129e785d1e787e9fc376a9041c56
Grace-Davanzo <mgd2157@columbia.edu>
2025-07-21 21:14:02 -0400

    Peace

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from Instruments import oscilloscope_rigol
 import pyvisa
-import vxi11
+
 from network_manager import NetworkManager
 
 rm = pyvisa.ResourceManager()
 insturment_list = [] #types the name of the instruments you want to query 
 print(rm.list_resources())
 print(rm)
-inst = vxi11.Instrument("TCPIP0::128.59.65.98::INSTR")
+#inst = rm.open_resource("TCPIP0::128.59.65.98::INSTR")
 nm = NetworkManager()
 #nm.add_new_instrument("fake", rm.list_resources(),rm)
 #Add auto connection
 #r = rm.open_resource('USB0::0x1AB1::0x0517::DS1ZE264M00036::INSTR')
 #ro = oscilloscope_rigol.Oscilloscope(r)
 #inst = rm.open_resource('TCPIP0::128.59.65.98::INSTR')
-id = inst.ask('*IDN')
+#id = inst.ask('*IDN')
