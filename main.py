bf9314da2a9027d433cd5e429fc85d348e66c2a3
Grace-Davanzo <mgd2157@columbia.edu>
2025-09-12 13:44:49 -0400

    FIxed switch

--- main.py
+++ main.py
@@ -5,12 +5,55 @@ from spectrum_analyzer_signal_hound import SpectrumAnalyzer
 from vector_network_analyzer_copper_mountain import VNA
 import time
 from network_manager import NetworkManager
-
+import pyvisa
+from EInstrument import EInstrument
 # Initialize the instruments
 #There will be some pop ups
 nm = NetworkManager()
+rm = pyvisa.ResourceManager()
 #dc = nm.connect_dc_power_supply()
-osc = nm.connect_oscilloscope()
+port = 'TCPIP0::192.168.0.8::8249::SOCKET'
+rf = rm.open_resource(port)
+rf.read_termination = '\n'
+rf.write_termination = '\n'
+rf.write("Reset")
+time.sleep(0.3)
+print("Both channel status "+str(rf.write("Read_Channel_Status?")))
+print(rf.write("CH1_Status?"))
+time.sleep(0.3)
+rf.write("CH1_1_ON")
+time.sleep(0.3)
+print("Both channel status "+str(rf.write("Read_Channel_Status?")))
+print(rf.write("CH1_Status?"))
+time.sleep(0.3)
+rf.write("CH1_1_OFF")
+time.sleep(0.3)
+print("Both channel status "+str(rf.write("Read_Channel_Status?")))
+time.sleep(0.3)
+print(rf.write("CH1_Status?"))
+time.sleep(0.3)
+rf.write("CH1_5_ON")
+time.sleep(0.3)
+print("Both channel status "+str(rf.write("Read_Channel_Status?\r")))
+time.sleep(0.3)
+print(rf.query("CH1_Status?\r"))
+time.sleep(0.3)
+rf.write("CH1_5_OFF")
+time.sleep(0.3)
+print("Both channel status "+str(rf.query("Read_Channel_Status?")))
+time.sleep(0.3)
+print(rf.write("CH1_Status"))
+
+'''rfs = nm.connect_rf_switch()
+rfs.switch1.channel_5.disable()
+print(rfs.switch1.get_status())
+rfs.switch1.channel_1.enable()
+print(rfs.switch1.get_status())
+rfs.switch1.channel_1.disable()
+print(rfs.switch1.get_status())
+rfs.switch1.channel_5.enable()
+print(rfs.switch1.get_status())'''
+"""osc = nm.connect_oscilloscope()
 vna = nm.connect_vector_network_analyzer()
 sa = nm.connect_spectrum_analyzer()
 fm = nm.connect_flowmeter()
@@ -32,4 +75,4 @@ sa.sense.bluetooth.set_channel_index(1)
 # Data Processing
 print(data)
 #Disconnect all instruments and close applications
-nm.disconnect([osc,vna,sa,fm])
+nm.disconnect([osc,vna,sa,fm])"""
--- rf_switch.py
+++ rf_switch.py
@@ -33,12 +33,12 @@ class Switch:
         self.instrument = instrument
         self.data_handler = data_handler
         self.switch = switch
-        self.channel1 = Channel(instrument, data_handler, switch, 1)
-        self.channel2 = Channel(instrument, data_handler, switch, 2)
-        self.channel3 = Channel(instrument, data_handler, switch, 3)
-        self.channel4 = Channel(instrument, data_handler, switch, 4)
-        self.channel5 = Channel(instrument, data_handler, switch, 5)
-        self.channel6 = Channel(instrument, data_handler, switch, 6)
+        self.channel_1 = Channel(instrument, data_handler, switch, 1)
+        self.channel_2 = Channel(instrument, data_handler, switch, 2)
+        self.channel_3 = Channel(instrument, data_handler, switch, 3)
+        self.channel_4 = Channel(instrument, data_handler, switch, 4)
+        self.channel_5 = Channel(instrument, data_handler, switch, 5)
+        self.channel_6 = Channel(instrument, data_handler, switch, 6)
     def reset(self):
         """
         Reset the switch to its default state. If successfully returns 000000
@@ -65,7 +65,7 @@ class Channel:
         """
         status = self.get_status()
         #if status == '000000':
-        self.instrument.write(f"CH{self.switch}_{self.channel}_ON")
+        self.instrument.write(f"{self.switch}_{self.channel}_ON")
         #else:
             #print(f"Channel {status.index('1')} is already on. Please disable it or reset the switch before turning on channel {self.channel}")
     
@@ -73,7 +73,7 @@ class Channel:
         """
         Enable the channel for this switch.
         """
-        self.instrument.write(f"CH{self.switch}_{self.channel}_OFF")
+        self.instrument.write(f"{self.switch}_{self.channel}_OFF")
 
     def get_status(self):
         """Get which switch channels are enabled.
