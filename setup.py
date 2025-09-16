19d29486644d1d8f277c107428cd02057ac85b7c
Grace-Davanzo <mgd2157@columbia.edu>
2025-09-15 22:48:41 -0400

    Version changed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(work_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='measurement_instruments',
-    version='0.0.9',
+    version='0.1.1',
     url='https://github.com/phoqis-lab/Measurement_Software',
     author='Mary Grace D Avanzo',
     author_email='mgd2157@columbia.edu',
